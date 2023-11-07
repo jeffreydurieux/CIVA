3f171392e89bbc60839131bb45e03a2f6d6c1519
Jeffrey Durieux <jeffreydurieux@users.noreply.github.com>
2023-11-07 11:08:21 +0100

    add SearchEmptyClusters
    Add script for dealing with empty clusters

--- CIVA.R
+++ CIVA.R
@@ -2,23 +2,24 @@
 # JD
 # What: first basic version of CIVA
 
-library(ivaBSS)
-library(multiway)
-library(abind)
-library(NMFN)
-source('EstAih.R')
-source('fastIVA_custom.R')
-source('Sim_CIVA.R')
-source('whiten_custom.R')
-source('clusf.R')
-source('invW_Asplit.R')
-source('ConcData.R')
-source('ExtractIVA.R')
-source('Lir.R')
-source('Reclus.R')
-source('Xscale.R')
+# library(ivaBSS)
+# library(multiway)
+# library(abind)
+# library(NMFN)
+# source('EstAih.R')
+# source('fastIVA_custom.R')
+# source('Sim_CIVA.R')
+# source('whiten_custom.R')
+# source('clusf.R')
+# source('invW_Asplit.R')
+# source('ConcData.R')
+# source('ExtractIVA.R')
+# source('Lir.R')
+# source('Reclus.R')
+# source('Xscale.R')
+# source('CIVA.R')
 
-CIVA <- function(X, nc, R, starts, parallel = FALSE, verbose = TRUE){
+CIVA <- function(X, nc, R, starts, parallel = FALSE, verbose = TRUE, initP = NULL){
  
   X <- lapply(X, xscale) 
   
@@ -34,10 +35,15 @@ CIVA <- function(X, nc, R, starts, parallel = FALSE, verbose = TRUE){
       #1 init P
       if(iter == 1){
         Losstotalstart <- Losstotal
-        P <- clusf(nBlocks = length(X), nClus = R)
+        if(is.null(initP)){
+          P <- clusf(nBlocks = length(X), nClus = R)  
+        }else{
+          P <- initP
+        }
+        
         DataL <- ConcData(X,P)
       }else{
-        DataL <- ConcData(X,UpdatePInfo$newclus)
+        DataL <- ConcData(X,newclus)
       }
       
       #2 extract IVA
@@ -45,6 +51,12 @@ CIVA <- function(X, nc, R, starts, parallel = FALSE, verbose = TRUE){
       
       #3 Reclus
       UpdatePInfo <- Reclus(DataList = X, QrL = IVAL$Qr)
+      
+      newclus <- SearchEmptyClusters(nClus = R,
+                                     newcluster = UpdatePInfo$newclus,
+                                     SSminVec = UpdatePInfo$SSminVec)
+      
+      table(UpdatePInfo$newclus)
       Losstotalstart<- c(Losstotalstart,UpdatePInfo$Loss)
       
       if(verbose == TRUE){
@@ -53,12 +65,16 @@ CIVA <- function(X, nc, R, starts, parallel = FALSE, verbose = TRUE){
       
       #4 Convergence
       criterion <- tail(Losstotalstart, n = 2)
-      if(criterion[2] == criterion[1]){
-        cat('Convergence of start ', i, '\n')
+      if( (criterion[1] - criterion[2]) < 1e-6 ){
+        if(verbose){
+          cat('Convergence of start ', i, '\n')  
+        }
+        
         start <- list()
         start$P <- UpdatePInfo$newclus
         start$IVA <- IVAL
         start$LossStart <- UpdatePInfo$Loss
+        start$Lossall <- Losstotalstart 
         break()
       }
       
@@ -74,76 +90,76 @@ CIVA <- function(X, nc, R, starts, parallel = FALSE, verbose = TRUE){
    return(allstarts)
 }
 
-set.seed(2407*3)
-Ti <- 10
-starts <- 10
-Q <- 5
-testdata <- Simulate_CIVA(Nr = 20, V = 1000, Ti =Ti, Q = Q, R = 2,D = 3, E = 0.01)
-
-tmp <- proc.time()
-testciva <- CIVA(X = testdata$Xe, nc = Q, R = 2, starts = starts)
-time <- proc.time() - tmp
-time
-
-Pests <- sapply(seq_along(testciva), function(x) testciva[[x]]$P)
-Pests
-ARI_starts <- apply(Pests, MARGIN = 2, mclust::adjustedRandIndex, y = testdata$P)
-ARI_starts
-cat(length( which(ARI_starts==1) ) /starts * 100,'%')
-
-losses <- sapply(seq_along(testciva), function(x) testciva[[x]]$LossStart)
-
-Optimal <- which.min(losses)
-
-OptStart <- testciva[[Optimal]]
-
-ARI <- mclust::adjustedRandIndex(testdata$P, OptStart$P)
-
-#source('https://raw.githubusercontent.com/jeffreydurieux/usefulcode/master/FindOptimalClusPermut.R')
-#source('https://raw.githubusercontent.com/jeffreydurieux/usefulcode/master/FindOptimalPermutSources.R')
-#library(gtools)
-p_per <- FindOptimalClusPermut(OptStart$P, testdata$P)
-
-# recov Q1
-SresTuck <- mean(
-  mean(c(
-FindOptimalPermutSingle( t(testdata$Qr[[ p_per$BestPerm[1] ]][,,1]) , t(OptStart$IVA$Qr[[1]][,,1]) )$BestRecov,
-FindOptimalPermutSingle( t(testdata$Qr[[ p_per$BestPerm[1] ]][,,2]) , t(OptStart$IVA$Qr[[1]][,,2]) )$BestRecov,
-FindOptimalPermutSingle( t(testdata$Qr[[ p_per$BestPerm[1] ]][,,3]) , t(OptStart$IVA$Qr[[1]][,,3]) )$BestRecov)
-)
-,
-# recov Q2
-mean(c(
-  FindOptimalPermutSingle( t(testdata$Qr[[ p_per$BestPerm[2] ]][,,1]) , t(OptStart$IVA$Qr[[2]][,,1]) )$BestRecov,
-  FindOptimalPermutSingle( t(testdata$Qr[[ p_per$BestPerm[2] ]][,,2]) , t(OptStart$IVA$Qr[[2]][,,2]) )$BestRecov,
-  FindOptimalPermutSingle( t(testdata$Qr[[ p_per$BestPerm[2] ]][,,3]) , t(OptStart$IVA$Qr[[2]][,,3]) )$BestRecov)
-)
-)
-Aest1 <- inv_W(OptStart$IVA$Wr[[1]])
-Aest1 <- Asplit(Aest1, dimsplit = Ti)
-
-Aest2 <- inv_W(OptStart$IVA$Wr[[2]])
-Aest2 <- Asplit(Aest2, dimsplit = Ti)
-
-Ais1 <- numeric()
-for(i in 1:length(testdata$Ais[[ p_per$BestPerm[1]]]) ){
-Ais1[i] <- mean(c(
-  FindOptimalPermutSingle( testdata$Ais[[ p_per$BestPerm[1] ]][[i]][,,1] , Aest1[[i]][,,1] )$BestRecov,
-  FindOptimalPermutSingle( testdata$Ais[[ p_per$BestPerm[1] ]][[i]][,,2] , Aest1[[i]][,,2] )$BestRecov,
-  FindOptimalPermutSingle( testdata$Ais[[ p_per$BestPerm[1] ]][[i]][,,3] , Aest1[[i]][,,3] )$BestRecov)
-)  
-}
-
-Ais2 <- numeric()
-for(i in 1:length(testdata$Ais[[ p_per$BestPerm[2]]]) ){
-  Ais2[i] <- mean(c(
-    FindOptimalPermutSingle( testdata$Ais[[ p_per$BestPerm[2] ]][[i]][,,1] , Aest2[[i]][,,1] )$BestRecov,
-    FindOptimalPermutSingle( testdata$Ais[[ p_per$BestPerm[2] ]][[i]][,,2] , Aest2[[i]][,,2] )$BestRecov,
-    FindOptimalPermutSingle( testdata$Ais[[ p_per$BestPerm[2] ]][[i]][,,3] , Aest2[[i]][,,3] )$BestRecov)
-  )  
-}
-# Res A
-AresTuck <- mean(mean(Ais1), mean(Ais2))
-
-
-ARI; SresTuck; AresTuck
+# set.seed(2407*3)
+# Ti <- 10
+# starts <- 2
+# Q <- 5
+# testdata <- Simulate_CIVA(Nr = 20, V = 1000, Ti =Ti, Q = Q, R = 2,D = 3, E = 0.01)
+# 
+# tmp <- proc.time()
+# testciva <- CIVA(X = testdata$Xe, nc = Q, R = 2, starts = starts)
+# time <- proc.time() - tmp
+# time
+# 
+# Pests <- sapply(seq_along(testciva), function(x) testciva[[x]]$P)
+# Pests
+# ARI_starts <- apply(Pests, MARGIN = 2, mclust::adjustedRandIndex, y = testdata$P)
+# ARI_starts
+# cat(length( which(ARI_starts==1) ) /starts * 100,'%')
+# 
+# losses <- sapply(seq_along(testciva), function(x) testciva[[x]]$LossStart)
+# 
+# Optimal <- which.min(losses)
+# 
+# OptStart <- testciva[[Optimal]]
+# 
+# ARI <- mclust::adjustedRandIndex(testdata$P, OptStart$P)
+# 
+# #source('https://raw.githubusercontent.com/jeffreydurieux/usefulcode/master/FindOptimalClusPermut.R')
+# #source('https://raw.githubusercontent.com/jeffreydurieux/usefulcode/master/FindOptimalPermutSources.R')
+# #library(gtools)
+# p_per <- FindOptimalClusPermut(OptStart$P, testdata$P)
+# 
+# # recov Q1
+# SresTuck <- mean(
+#   mean(c(
+# FindOptimalPermutSingle( t(testdata$Qr[[ p_per$BestPerm[1] ]][,,1]) , t(OptStart$IVA$Qr[[1]][,,1]) )$BestRecov,
+# FindOptimalPermutSingle( t(testdata$Qr[[ p_per$BestPerm[1] ]][,,2]) , t(OptStart$IVA$Qr[[1]][,,2]) )$BestRecov,
+# FindOptimalPermutSingle( t(testdata$Qr[[ p_per$BestPerm[1] ]][,,3]) , t(OptStart$IVA$Qr[[1]][,,3]) )$BestRecov)
+# )
+# ,
+# # recov Q2
+# mean(c(
+#   FindOptimalPermutSingle( t(testdata$Qr[[ p_per$BestPerm[2] ]][,,1]) , t(OptStart$IVA$Qr[[2]][,,1]) )$BestRecov,
+#   FindOptimalPermutSingle( t(testdata$Qr[[ p_per$BestPerm[2] ]][,,2]) , t(OptStart$IVA$Qr[[2]][,,2]) )$BestRecov,
+#   FindOptimalPermutSingle( t(testdata$Qr[[ p_per$BestPerm[2] ]][,,3]) , t(OptStart$IVA$Qr[[2]][,,3]) )$BestRecov)
+# )
+# )
+# Aest1 <- inv_W(OptStart$IVA$Wr[[1]])
+# Aest1 <- Asplit(Aest1, dimsplit = Ti)
+# 
+# Aest2 <- inv_W(OptStart$IVA$Wr[[2]])
+# Aest2 <- Asplit(Aest2, dimsplit = Ti)
+# 
+# Ais1 <- numeric()
+# for(i in 1:length(testdata$Ais[[ p_per$BestPerm[1]]]) ){
+# Ais1[i] <- mean(c(
+#   FindOptimalPermutSingle( testdata$Ais[[ p_per$BestPerm[1] ]][[i]][,,1] , Aest1[[i]][,,1] )$BestRecov,
+#   FindOptimalPermutSingle( testdata$Ais[[ p_per$BestPerm[1] ]][[i]][,,2] , Aest1[[i]][,,2] )$BestRecov,
+#   FindOptimalPermutSingle( testdata$Ais[[ p_per$BestPerm[1] ]][[i]][,,3] , Aest1[[i]][,,3] )$BestRecov)
+# )  
+# }
+# 
+# Ais2 <- numeric()
+# for(i in 1:length(testdata$Ais[[ p_per$BestPerm[2]]]) ){
+#   Ais2[i] <- mean(c(
+#     FindOptimalPermutSingle( testdata$Ais[[ p_per$BestPerm[2] ]][[i]][,,1] , Aest2[[i]][,,1] )$BestRecov,
+#     FindOptimalPermutSingle( testdata$Ais[[ p_per$BestPerm[2] ]][[i]][,,2] , Aest2[[i]][,,2] )$BestRecov,
+#     FindOptimalPermutSingle( testdata$Ais[[ p_per$BestPerm[2] ]][[i]][,,3] , Aest2[[i]][,,3] )$BestRecov)
+#   )  
+# }
+# # Res A
+# AresTuck <- mean(mean(Ais1), mean(Ais2))
+# 
+# 
+# ARI; SresTuck; AresTuck
---
+++ SearchEmptyClusters.R
@@ -0,0 +1,79 @@
+# Tue Nov  7 10:33:48 2023 ------------------------------
+# JD
+
+# What: search empty clusters for CIVA
+
+SearchEmptyClusters <- function(nClus, newcluster, SSminVec = NULL) {
+  
+  if(is.null(SSminVec)){
+    SSminVec <- rep(100, length(newcluster))
+  }
+  
+  OriCluster <- 1:nClus
+  
+  test <- sapply(OriCluster, FUN = '%in%', newcluster)
+  
+  #test result = no empty clusters so return original newcluster
+  
+  if ( all( test == TRUE) ){
+    newcluster <- newcluster
+  }else{
+    
+    # check singletons, these are not candidates to choose from
+    EmptyClusters <- which(test == FALSE)
+    singletonnames <- names(which( (table(newcluster)  == 1) == TRUE))
+    singletons <- as.numeric(singletonnames)
+    id <- which(newcluster %in% singletons == T)
+    
+    SSminVec[id] <- 0
+    
+    
+    worst <- sort( SSminVec, decreasing = TRUE)
+    
+    #remove worst of singletons, otherwise empties will occur
+    
+    Index <- sapply( seq_along(EmptyClusters),
+                     function(i) FUN = which( SSminVec == worst[i] ) )
+    
+    # if ties occur in SSminVec, pick one at random
+    if( is.null(ncol(Index)) == FALSE ){
+      Index <- Index[,1]
+      Index <- sample(Index, size = length(EmptyClusters))
+    }
+    
+    for(i in 1:length(Index)){
+      newcluster <- replace(newcluster, Index[i], EmptyClusters[i])
+      
+    }
+    
+    
+  }# else some emptyclusters
+  
+  if( length(unique(newcluster)) != nClus ){
+    stop('In function SearchEmptyCluster, empty/ies occurred')
+  }
+  
+  
+  # check min number of clusters
+  # if(!is.null(minclus)){
+  #   test2 <- table(newcluster) < minclus
+  #
+  #   if ( all( test2 == TRUE) ){
+  #     newcluster <- newcluster
+  #   }else{
+  #     SparseClusters <- which(test2 == TRUE)
+  #
+  #     worst <- sort( SSminVec, decreasing = TRUE)
+  #
+  #     Index <- sapply( seq_along(1:minclus),
+  #                      function(i) FUN = which( SSminVec == worst[i] ) )
+  #     newcluster[Index] <- SparseClusters
+  #   }
+  # }
+  
+  
+  
+  return(newcluster)
+}
+
+test <- UpdatePInfo$SSList
